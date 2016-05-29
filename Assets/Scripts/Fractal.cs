3050a4d48211aa25ed921b2ef1c5682bb012320c
Kristian Sakarisson <kristian@sakarisson.com>
2016-05-29 18:18:55 +0100

    Added material file, added some code to Fractal.cs

--- Assets/Scripts/Fractal.cs
+++ Assets/Scripts/Fractal.cs
@@ -3,13 +3,39 @@ using System.Collections;
 
 public class Fractal : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
+    public Mesh mesh;
+    public Material material;
+
+    public int maxDepth;
+    private int depth;
+
+    private void Start ()
+    {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+
+        if (depth < maxDepth)
+        {
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.up);
+
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.right);
+
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public float childScale;
+
+    private void Initialize(Fractal parent, Vector3 direction)
+    {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        childScale = parent.childScale;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+        transform.localPosition = direction * (.5f + .5f * childScale);
+    }
 }
